bc50aa8c49a525ee27bb571e857f0fd992b2c93d
Tim Stack <tstack43215@yahoo.com>
2023-02-10 15:54:07 -0600

    Initial Commit

---
+++ Koch Snowflake/Koch_SnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koch_SnowflakeApp.swift
+//  Koch Snowflake
+//
+//  Created by IIT PHYS 440 on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Koch_SnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
